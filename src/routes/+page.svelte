9a24a1fc213052d05ab832e365dc041463da5adc
Jo Kroese <jo@jokroese.com>
2025-11-22 13:04:19 -0400

    feat: add examples selector to toolbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,33 @@
 	import HydraCanvas from '$lib/components/HydraCanvas.svelte';
 	import type { Issue } from '$lib/engine/HydraEngine.js';
 	import { defaultExample } from '$lib/examples/default.js';
+	import type { ExamplePatch } from '$lib/examples/types.js';
 	import type { IREdge, IRNode } from '$lib/types.js';
 
 	let nodes = $state.raw<IRNode[]>(defaultExample.nodes);
 	let edges = $state.raw<IREdge[]>(defaultExample.edges);
+
+	function loadExample(ex: ExamplePatch) {
+		// Ensure fresh ids to avoid collisions if user already added stuff
+		// eslint-disable-next-line svelte/prefer-svelte-reactivity
+		const idMap = new Map<string, string>();
+		const remapId = (oldId: string) => {
+			if (!idMap.has(oldId)) idMap.set(oldId, nanoid());
+			return idMap.get(oldId)!;
+		};
+
+		nodes = ex.nodes.map((n) => ({
+			...n,
+			id: remapId(n.id)
+		}));
+
+		edges = ex.edges.map((e) => ({
+			...e,
+			id: nanoid(),
+			source: remapId(e.source),
+			target: remapId(e.target)
+		}));
+	}
 
 	function addNode(node: Omit<IRNode, 'id'>): string {
 		const id = nanoid();
@@ -34,5 +57,12 @@
 <div class="w-full h-screen bg-black relative">
 	<HydraCanvas {nodes} {edges} onValidationIssues={(issues) => (validationIssues = issues)} />
 
-	<FlowEditor bind:nodes bind:edges {addNode} {updateNodeData} {validationIssues} />
+	<FlowEditor
+		bind:nodes
+		bind:edges
+		{addNode}
+		{updateNodeData}
+		{validationIssues}
+		onSelectExample={loadExample}
+	/>
 </div>
